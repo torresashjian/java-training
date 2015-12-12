83dfa6e7e3e67199e4698401fa773cb7a2632a01
Miguel Torres <mtorres@tibco.com>
2015-12-11 19:52:52 -0800

    Finish mergesort java

--- src/sorting/MergeSort.java
+++ src/sorting/MergeSort.java
@@ -4,15 +4,57 @@
 **/
 package sorting;
 
+import java.util.Arrays;
+
 public class MergeSort {
 	
 	/**
 	* Sorts an array using merge sort algorithm	
 	*/
 	public int[] mergeSort(int[] arr){
-		System.out.println("Inside Merge sort");
-		
-		return new int[]{3,4};
+		int len = arr.length;
+		if(len > 1){
+			// break in 2
+			int mid = len / 2;
+			int [] left = mergeSort(Arrays.copyOfRange(arr, 0, mid));
+			int [] right = mergeSort(Arrays.copyOfRange(arr, mid, len));
+			// Merge left and right
+			return merge(left, right);
+		}
+		return arr;
+	}
+	
+	private int[] merge(int[] left, int[] right){
+		int totalLength = left.length + right.length;
+		int[] mergedArr = new int[totalLength];
+		int count = 0;
+		int leftIndex = 0;
+		int rightIndex = 0;
+		while(count < totalLength){
+			// Compare left and right elements
+			if(leftIndex >= left.length){
+				// Add right part to new array
+				mergedArr[count] = right[rightIndex];
+				rightIndex++;
+			} else if(rightIndex >= right.length){
+				// Add left to new array
+				mergedArr[count] = left[leftIndex];
+				leftIndex++;
+			} else if(left[leftIndex] < right[rightIndex]){
+				// Add left to new array
+				mergedArr[count] = left[leftIndex];
+				// Increase left counter
+				leftIndex++;
+			} else {
+				// Add right to new array
+				mergedArr[count] = right[rightIndex];
+				// Increase left counter
+				rightIndex++;
+			}
+			// Increment counter
+			count ++;
+		}
+		return mergedArr;
 	}
 	
 }
--- src/sorting/MergeSortTest.java
+++ src/sorting/MergeSortTest.java
@@ -9,10 +9,35 @@ public class MergeSortTest {
 	
 	@Test
 	public void testMergeSort(){
-		System.out.println("Inside test");
+		System.out.println("Inside test testMergeSort");
 		final MergeSort ms = new MergeSort();
 		// Create array to sort
-		int[] unsortedArr = new int[]{6,5,3,1,8,7,2,4};
+		int[] unsortedArr = new int[]{6,5,3,1,8,7,2,4,4};
+		int[] sortedArr = ms.mergeSort(unsortedArr);
+		System.out.println("Printing sorted arr");
+		for(int element: sortedArr){
+			System.out.println(element);
+		}
+	}
+	@Test
+	public void testMergeSortEmpty(){
+		System.out.println("Inside test testMergeSortEmpty");
+		final MergeSort ms = new MergeSort();
+		// Create array to sort
+		int[] unsortedArr = new int[]{};
+		int[] sortedArr = ms.mergeSort(unsortedArr);
+		System.out.println("Printing sorted arr");
+		for(int element: sortedArr){
+			System.out.println(element);
+		}
+	}
+	
+	@Test
+	public void testMergeSortOne(){
+		System.out.println("Inside test testMergeSortOne");
+		final MergeSort ms = new MergeSort();
+		// Create array to sort
+		int[] unsortedArr = new int[]{2};
 		int[] sortedArr = ms.mergeSort(unsortedArr);
 		System.out.println("Printing sorted arr");
 		for(int element: sortedArr){
